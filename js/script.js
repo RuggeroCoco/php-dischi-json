be54be5a6a3fbbeb26b2858e0f1a526bcd5261a8
RuggeroCoco <ruggerococo94@gmail.com>
2023-05-12 20:07:58 +0200

    End of project

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-            disks: [],
-        };
+  data() {
+    return {
+      disks: [],
+    };
+  },
+  mounted() {
+    this.getDisks();
+  },
+  methods: {
+    getDisks() {
+      axios
+        .get("http://localhost/Boolean-progetti/php-dischi-json/server.php")
+        .then((resp) => {
+          this.disks = resp.data.results;
+          console.log(resp);
+        });
     },
-    mounted() {
-        this.getDisks();
-    },
-    methods: {
-        getDisks() {
-            axios.get("http://localhost/Boolean-progetti/php-dischi-json/server.php")
-            .then((resp) => {
-                this.disks = resp.data.results;
-                console.log(resp);
-            });
-        },
-    },
+  },
 }).mount("#app");
--- server.php
+++ server.php
@@ -2,6 +2,13 @@
 $dischi_str = file_get_contents("dischi.json");
 $dischi = json_decode($dischi_str, true);
 
+if (isset($_GET["disk_id"])) {
+    $id = $_GET["disk_id"];
+    $rsult = $dischi[$id];
+} else {
+    $result = $dischi;
+}
+
 $result = [
     "results" => $dischi
 ];
