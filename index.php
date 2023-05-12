566961042f7a953d822c437775fce99303661aa1
RuggeroCoco <ruggerococo94@gmail.com>
2023-05-12 19:41:10 +0200

    Add Card

--- index.php
+++ index.php
@@ -24,12 +24,12 @@
         <div class="container my-4">
             <div class="row row-cols-4 g-4">
                 <div class="col" v-for="(disk,index) in disks" :key="index">
-                    <div class="card">
+                    <div class="card text-center">
                         <img :src="disk.poster" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <h5 class="card-title">{{disk.title}}</h5>
+                            <p class="card-text">{{disk.author}}</p>
+                            <a href="#" class="btn btn-primary">Details</a>
                         </div>
                     </div>
 
